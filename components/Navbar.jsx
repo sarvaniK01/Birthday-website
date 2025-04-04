181092d32bb7c380da027767ce007398af13194a
sarvani-kandukuri_pwcit <sarvani.kandukuri@net-effect.com>
2025-04-04 15:23:00 +0530

    Changed Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,19 +5,19 @@ const Navbar = () => {
         <nav className="bg-lightPink px-40 py-10">
             <ul className="flex justify-between">
                 <Link href="/">
-                    <li className="font-merriWeather text-4xl font-bold text-white">Homepage</li>
+                    <li className="font-merriWeather text-3xl font-bold text-white nav-item">Homepage</li>
                 </Link>
                 <Link href="/years">
-                    <li className="font-merriWeather text-4xl font-bold text-white">Over The years</li>
+                    <li className="font-merriWeather text-3xl font-bold text-white nav-item">Over The years</li>
                 </Link>
                 <Link href="/messages">
-                    <li className="font-merriWeather text-4xl font-bold text-white">Birthday Messages</li>
+                    <li className="font-merriWeather text-3xl font-bold text-white nav-item">Birthday Messages</li>
                 </Link>
                 <Link href="/neverAlone">
-                    <li className="font-merriWeather text-4xl font-bold text-white">You're Never Alone</li>
+                    <li className="font-merriWeather text-3xl font-bold text-white nav-item">You're Never Alone</li>
                 </Link>
                 <Link href="/emailMe">
-                    <li className="font-merriWeather text-4xl font-bold text-white">Email Me</li>
+                    <li className="font-merriWeather text-3xl font-bold text-white nav-item">Email Me</li>
                 </Link>
             </ul>
         </nav>
